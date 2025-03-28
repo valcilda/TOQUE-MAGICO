5c7faccba160aadf9141fe65879f1f4603ba4590
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2025-03-27 19:58:08 -0500

    VERSÃO 1.3.2
    ❇️ Novo Layout Redesigned: O design do site foi modernizado com o *MaterializeCSS*, garantindo uma interface mais limpa, intuitiva e responsiva.
    ❇️ Novas Seções:
      🔸Home: Página principal com cabeçalho adaptável, slider com produtos e serviços, destaque para mercadorias (películas, adesivos e pedrarias) e a seção sobre a empreendedora Valcilda. O rodapé contém links para redes sociais e crédito de criação.
      🔸Localização: Seção com o endereço completo, coordenadas geográficas e link para o Google Maps, além de um botão para voltar à página anterior.
      🔸Preços: Tabela organizada de preços, dividida entre preços regulares e no atacado, com botão de navegação fácil.
      🔸Sobre: Página que explica o projeto "Acessórios para Adesivos de Unhas", com uma galeria de imagens dos materiais e objetivo de tornar a aplicação dos adesivos mais prática e profissional.

--- CODIGO/01_HOME/js/script.js
+++ CODIGO/01_HOME/js/script.js
@@ -2,3 +2,5 @@ $(document).ready(function(){
     $(".button-collapse").sideNav(); 
     $('.slider').slider();     
 });  
+
+document.getElementById("idade").textContent = new Date().getFullYear() - 1976;
